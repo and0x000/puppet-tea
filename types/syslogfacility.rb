818d42f19a77f4f8acdcacbe6f7101735631a283
b4ldr <github@johnbond.org>
2017-06-07 15:58:00 +0100

    add syslog facility

---
+++ types/syslogfacility.rb
@@ -0,0 +1,19 @@
+type Tea::Syslogfacility = Enum[
+  'kern',
+  'user',
+  'mail',
+  'daemon',
+  'auth',
+  'lpr',
+  'news',
+  'uucp',
+  'cron',
+  'local0',
+  'local1',
+  'local2',
+  'local3',
+  'local4',
+  'local5',
+  'local6',
+  'local7'
+]
